f7ce35ba7e993261f1c82ef2e90434c1b90ef7e9
2019082851 <2019082851@hanyang.ac.kr>
2023-12-10 14:13:30 +0900

    [feat] 가게 미션 목록 조회 API 구현

--- src/main/java/study/converter/MissionConverter.java
+++ src/main/java/study/converter/MissionConverter.java
@@ -1,11 +1,15 @@
 package study.converter;
 
+import org.springframework.data.domain.Page;
 import study.domain.Mission;
 import study.domain.Review;
 import study.web.dto.MissionRequestDTO;
 import study.web.dto.MissionResponseDTO;
 import study.web.dto.ReviewRequestDTO;
 import study.web.dto.ReviewResponseDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MissionConverter {
     public static MissionResponseDTO.MissionJoinResultDTO toJoinResultDTO(Mission mission) {
@@ -20,6 +24,30 @@ public class MissionConverter {
                 .missionSpec(request.getMissionSpec())
                 .deadline(request.getDeadline())
                 .reward(request.getReward())
+                .build();
+    }
+
+    public static MissionResponseDTO.MissionPreViewDTO missionPreViewDTO(Mission mission){
+        return MissionResponseDTO.MissionPreViewDTO.builder()
+                .missionId(mission.getMissionId())
+                .createdAt(mission.getCreatedAt().toLocalDate())
+                .updateAt(mission.getUpdatedAt().toLocalDate())
+                .deadline(mission.getDeadline())
+                .missionSpec(mission.getMissionSpec())
+                .build();
+    }
+    public static MissionResponseDTO.MissionPreviewListDTO reviewPreViewListDTO(Page<Mission> missionList){
+
+        List<MissionResponseDTO.MissionPreViewDTO> missionPreViewDTOList = missionList.stream()
+                .map(MissionConverter::missionPreViewDTO).collect(Collectors.toList());
+
+        return MissionResponseDTO.MissionPreviewListDTO.builder()
+                .isLast(missionList.isLast())
+                .isFirst(missionList.isFirst())
+                .totalPage(missionList.getTotalPages())
+                .totalElements(missionList.getTotalElements())
+                .listSize(missionPreViewDTOList.size())
+                .missionList(missionPreViewDTOList)
                 .build();
     }
 
--- src/main/java/study/service/StoreQueryService.java
+++ src/main/java/study/service/StoreQueryService.java
@@ -1,6 +1,7 @@
 package study.service;
 
 import org.springframework.data.domain.Page;
+import study.domain.Mission;
 import study.domain.Review;
 import study.domain.Store;
 
@@ -10,4 +11,6 @@ public interface StoreQueryService {
     Store findStore(Long id);
 
     Page<Review> getReviewList(Long storeId, Integer page);
+
+    Page<Mission> getMissionList(Long storeId, Integer page);
 }
